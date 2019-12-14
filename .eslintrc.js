35360cacd8e9f5be6d7ef2522fb395b8c74918ba
caokangkang <hkd_ckk@163.com>
2019-12-14 12:33:01 +0800

    从新初始化eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,17 @@
 module.exports = {
-    "extends": ["eslint:recommended"],
-    "rules": {
-        "no-console": ["error", {
-            "allow": ["warn", "error", "info"]
-        }]
-    },
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "ecmaVersion": 6,
-        "sourceType": "script"
-    },
-    "globals": {},
     "env": {
-        "node": true,
-        "es6": true,
-        "mocha": true
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
     }
-}
+};
