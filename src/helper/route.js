f76dc818ab0b4a637e3cab41d0971ac3214f643e
caokangkang <hkd_ckk@163.com>
2019-12-17 19:09:37 +0800

    添加 文件读取

--- src/helper/route.js
+++ src/helper/route.js
@@ -1,7 +1,13 @@
 const fs = require("fs");
+const path = require("path");
 const promisify = require('util').promisify;
+const Handlebars = require("handlebars");
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir)
+const tplPath = path.join(__dirname, "../template/dir.tpl")
+const source = fs.readFileSync(tplPath, "utf-8");
+const template = Handlebars.compile(source);
+const config = require("../config/defaultConfig.js")
 module.exports = async function (req, res, filePath) {
     try{
         const stats = await stat(filePath);
@@ -12,8 +18,14 @@ module.exports = async function (req, res, filePath) {
         }else if(stats.isDirectory()){
             const files = await readdir(filePath);
             res.statusCode = 200;
-            res.setHeader("Content-Type", 'text/plain');
-            res.end(files.join(","));
+            res.setHeader("Content-Type", 'text/html');
+            const dir = path.relative(config.root, filePath)
+            const data = {
+                title: path.basename(filePath),
+                dir: dir ? `/${dir}` : '',
+                files
+            }
+            res.end(template(data));
         }
     }catch(ex){
         res.statusCode = 404;
